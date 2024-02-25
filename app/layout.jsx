b3a15b6ca7897243afc595c93c720554255159dc
Rits113116 <rr4618634@gmail.com>
2024-02-25 17:26:49 +0530

    complete home page with authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 export const metadata={
     title:"Promptia",
     description:"Discover and share AI prompts"
@@ -7,14 +9,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'>
                 </div>
             </div>
                     <main className='app'>
+                        <Nav/>
                         {children}
                     </main>
-             
+                </Provider>
         </body>
     </html>
   )
